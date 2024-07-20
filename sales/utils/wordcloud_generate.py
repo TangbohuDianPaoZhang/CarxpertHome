2dff454bfce6c9e4916698b914bf70830139e46c
TangbohuDianPaoZhang <93420604+TangbohuDianPaoZhang@users.noreply.github.com>
2024-07-20 09:21:13 +0800

    word cloud img generate

---
+++ sales/utils/wordcloud_generate.py
@@ -0,0 +1,20 @@
+import json
+import jieba
+import numpy as np
+from PIL import Image
+from wordcloud import WordCloud
+from .get_data_by_month import *
+
+
+def generate_img():
+    cars = list(get_cars_by_month())
+    car_brand = ''
+    for i in cars:
+        car_brand += i.brand + ' '
+    words = jieba.lcut(car_brand)
+    words = ' '.join(words)
+    mask = np.array(Image.open('i8.png'))
+    wc = WordCloud(font_path='C:\\Windows\\Fonts\\msyh.ttc', background_color='White', max_words=100, mask=mask)
+    wc.generate(words)
+    wc.to_file('wordcloud-img/car_wordcloud.png')
+    return json.dumps({'status': 'success'})
--- sales/views.py
+++ sales/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse, JsonResponse
 from .utils import get_center_data
+from .utils import wordcloud_generate
 
 # Create your views here.
 
@@ -11,6 +12,7 @@ def center(request):
         month = request.GET.get('month', '')
         most_popular_car, most_popular_car_sales, max_car_type,max_car_type_count, max_brand, max_brand_count = get_center_data.get_center_data()
         roll_list = get_center_data.get_roll_list()
+        wordcloud_generate.generate_img()
         oil_rate, electric_rate, hybrid_rate = get_center_data.get_energe_type_rate()
         return JsonResponse({
             'most_popular_car': most_popular_car,
